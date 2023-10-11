9cc8f1597315b1f5b088e8a420f0a1a59fce73e4
rezaadit12 <rezaadit410@gmail.com>
2023-10-11 22:51:55 +0700

    addImgGallery

---
+++ 11_ImgGallery/script.js
@@ -0,0 +1,23 @@
+const thumbnail = document.querySelector('.container')
+const jumbo = document.querySelector('.jumbo')
+const thumbs = document.querySelectorAll('.thumb');
+
+
+
+thumbnail.addEventListener('click', function(e){
+    if(e.target.className == 'thumb'){
+        jumbo.src = e.target.src
+        jumbo.classList.add('fade');
+
+        setTimeout(function(){
+            jumbo.classList.remove('fade');
+        }, 300);
+
+        thumbs.forEach(function(thumb){
+            thumb.className = 'thumb';
+        });
+
+
+        e.target.classList.add('active')
+    }
+})
